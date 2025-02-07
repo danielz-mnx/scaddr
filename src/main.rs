e4d5c346f0fd58c17ddea9a9d3031f1a92d8e91a
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2025-02-07 13:27:50 +0200

    First version

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use serde::ser::Serializer;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Transaction {
+    transaction_type: String,
+    contract_name: String,
+    contract_address: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ScriptRun {
+    transactions: Vec<Transaction>,
+}
+
+impl Serialize for ScriptRun {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.collect_map(
+            self.transactions
+                .iter()
+                .filter(|t| t.transaction_type == "CREATE")
+                .map(|t| (t.contract_name.clone(), t.contract_address.clone())),
+        )
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+
+    let json_input = match std::fs::read_to_string(file_path) {
+        Ok(json) => json,
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    let script_run: ScriptRun = match serde_json::from_str(&json_input) {
+        Ok(script_run) => script_run,
+        Err(e) => {
+            eprintln!("Error parsing JSON: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    let json_output = serde_json::to_string_pretty(&script_run).unwrap();
+    print!("{}", json_output);
+}
